e317e95f5f7b59e0850729e042ac04310bfe6314
hunch1 <taylor1525@hotmail.com>
2023-02-27 18:20:48 -0500

    Initial commit

---
+++ osc10e-master/ch5/project/java/FCFS.java
@@ -0,0 +1,18 @@
+
+
+// I CREATED THIS !!!!!
+
+public class FCFS implements Algorithm {
+    @Override
+    public void schedule() {
+
+    }
+
+    @Override
+    public Task pickNextTask() {
+        return null;
+    }
+}
+
+
+
